0090d8c290040df352fd2560c1229c01ac886e31
Millad <millad.mg@gmail.com>
2024-06-09 11:13:59 -0700

    changes in structs; array_view; some progress on forward func

--- run.cpp
+++ run.cpp
@@ -18,6 +18,40 @@
 #include <iostream>
 #include <cassert>
 
+#include <optional>    // For std::optional
+
+template <typename Iterator>
+class array_view {
+    std::optional<Iterator> begin_;
+    std::optional<Iterator> end_;
+public:
+    array_view() : begin_(std::nullopt), end_(std::nullopt) {}
+    array_view(Iterator begin, Iterator end) : begin_(begin), end_(end) {}
+    Iterator begin() const {
+        assert(begin_.has_value() && "Accessing begin of an uninitialized array_view");
+        return *begin_;
+    }
+    Iterator end() const {
+        assert(end_.has_value() && "Accessing end of an uninitialized array_view");
+        return *end_;
+    }
+    size_t size() {
+        assert(begin_.has_value() && end_.has_value() && "Calculating size of an uninitialized array_view");
+        return *end_ - *begin_;
+    }
+
+    typename std::iterator_traits<Iterator>::reference
+    operator[](std::size_t index) {
+        assert(begin_.has_value() && "Accessing element of an uninitialized array_view");
+        assert(index >= 0 && "Index is out of range (less than zero)");
+        assert(index < size() && "Index is out of range (more than size)");
+        return (*begin_)[index];
+    }
+};
+
+#define MAKE_AV_VECTOR(T) array_view<std::vector<T>::iterator>
+
+
 // ----------------------------------------------------------------------------
 // Transformer model
 
@@ -78,8 +112,10 @@ struct RunState {
     std::vector<T> hb; // buffer for hidden dimension in the ffn (hidden_dim,)
     std::vector<T> hb2; // buffer for hidden dimension in the ffn (hidden_dim,)
     std::vector<T> q; // query (dim,)
-    std::vector<T> k; // key (dim,)
-    std::vector<T> v; // value (dim,)
+//    std::vector<T> k; // key (dim,)
+//    std::vector<T> v; // value (dim,)
+    T* k;
+    T* v;
     std::vector<T> att; // buffer for scores/attention values (n_heads, seq_len)
     std::vector<T> logits; // output logits
     // kv cache
@@ -211,7 +247,6 @@ void free_transformer(Transformer<T>* t) {
 }
 
 
-#if 0
 
 // ----------------------------------------------------------------------------
 // neural net blocks; the dynamics of the Transformer
@@ -265,13 +300,17 @@ void matmul(float* xout, float* x, float* w, int n, int d) {
     }
 }
 
-float* forward(Transformer* transformer, int token, int pos) {
+#if 0
+#endif
+
+template <typename T>
+float* forward(Transformer<T>* transformer, int token, int pos) {
 
     // a few convenience variables
     Config* p = &transformer->config;
     TransformerWeights* w = &transformer->weights;
-    RunState* s = &transformer->state;
-    float *x = s->x;
+    RunState<T>* s = &transformer->state;
+    float *x = s->x.data();
     int dim = p->dim;
     int kv_dim = (p->dim * p->n_kv_heads) / p->n_heads;
     int kv_mul = p->n_heads / p->n_kv_heads; // integer multiplier of the kv sharing in multiquery
@@ -286,17 +325,17 @@ float* forward(Transformer* transformer, int token, int pos) {
     for(unsigned long long l = 0; l < p->n_layers; l++) {
 
         // attention rmsnorm
-        rmsnorm(s->xb, x, w->rms_att_weight + l*dim, dim);
+        rmsnorm(s->xb.data(), x, w->rms_att_weight + l*dim, dim);
 
         // key and value point to the kv cache
         int loff = l * p->seq_len * kv_dim; // kv cache layer offset for convenience
-        s->k = s->key_cache + loff + pos * kv_dim;
-        s->v = s->value_cache + loff + pos * kv_dim;
+        s->k = s->key_cache.data() + loff + pos * kv_dim;
+        s->v = s->value_cache.data() + loff + pos * kv_dim;
 
         // qkv matmuls for this position
-        matmul(s->q, s->xb, w->wq + l*dim*dim, dim, dim);
-        matmul(s->k, s->xb, w->wk + l*dim*kv_dim, dim, kv_dim);
-        matmul(s->v, s->xb, w->wv + l*dim*kv_dim, dim, kv_dim);
+        matmul(s->q.data(), s->xb.data(), w->wq + l*dim*dim, dim, dim);
+        matmul(s->k, s->xb.data(), w->wk + l*dim*kv_dim, dim, kv_dim);
+        matmul(s->v, s->xb.data(), w->wv + l*dim*kv_dim, dim, kv_dim);
 
         // RoPE relative positional encoding: complex-valued rotate q and k in each head
         for (int i = 0; i < dim; i+=2) {
@@ -307,7 +346,7 @@ float* forward(Transformer* transformer, int token, int pos) {
             float fci = sinf(val);
             int rotn = i < kv_dim ? 2 : 1; // how many vectors? 2 = q & k, 1 = q only
             for (int v = 0; v < rotn; v++) {
-                float* vec = v == 0 ? s->q : s->k; // the vector to rotate (query or key)
+                float* vec = v == 0 ? s->q.data() : s->k; // the vector to rotate (query or key)
                 float v0 = vec[i];
                 float v1 = vec[i+1];
                 vec[i]   = v0 * fcr - v1 * fci;
@@ -315,88 +354,90 @@ float* forward(Transformer* transformer, int token, int pos) {
             }
         }
 
-        // multihead attention. iterate over all heads
-        int h;
-        #pragma omp parallel for private(h)
-        for (h = 0; h < p->n_heads; h++) {
-            // get the query vector for this head
-            float* q = s->q + h * head_size;
-            // attention scores for this head
-            float* att = s->att + h * p->seq_len;
-            // iterate over all timesteps, including the current one
-            for (int t = 0; t <= pos; t++) {
-                // get the key vector for this head and at this timestep
-                float* k = s->key_cache + loff + t * kv_dim + (h / kv_mul) * head_size;
-                // calculate the attention score as the dot product of q and k
-                float score = 0.0f;
-                for (int i = 0; i < head_size; i++) {
-                    score += q[i] * k[i];
-                }
-                score /= sqrtf(head_size);
-                // save the score to the attention buffer
-                att[t] = score;
-            }
-
-            // softmax the scores to get attention weights, from 0..pos inclusively
-            softmax(att, pos + 1);
-
-            // weighted sum of the values, store back into xb
-            float* xb = s->xb + h * head_size;
-            memset(xb, 0, head_size * sizeof(float));
-            for (int t = 0; t <= pos; t++) {
-                // get the value vector for this head and at this timestep
-                float* v = s->value_cache + loff + t * kv_dim + (h / kv_mul) * head_size;
-                // get the attention weight for this timestep
-                float a = att[t];
-                // accumulate the weighted value into xb
-                for (int i = 0; i < head_size; i++) {
-                    xb[i] += a * v[i];
-                }
-            }
-        }
-
-        // final matmul to get the output of the attention
-        matmul(s->xb2, s->xb, w->wo + l*dim*dim, dim, dim);
-
-        // residual connection back into x
-        for (int i = 0; i < dim; i++) {
-            x[i] += s->xb2[i];
-        }
-
-        // ffn rmsnorm
-        rmsnorm(s->xb, x, w->rms_ffn_weight + l*dim, dim);
-
-        // Now for FFN in PyTorch we have: self.w2(F.silu(self.w1(x)) * self.w3(x))
-        // first calculate self.w1(x) and self.w3(x)
-        matmul(s->hb, s->xb, w->w1 + l*dim*hidden_dim, dim, hidden_dim);
-        matmul(s->hb2, s->xb, w->w3 + l*dim*hidden_dim, dim, hidden_dim);
-
-        // SwiGLU non-linearity
-        for (int i = 0; i < hidden_dim; i++) {
-            float val = s->hb[i];
-            // silu(x)=x*σ(x), where σ(x) is the logistic sigmoid
-            val *= (1.0f / (1.0f + expf(-val)));
-            // elementwise multiply with w3(x)
-            val *= s->hb2[i];
-            s->hb[i] = val;
-        }
-
-        // final matmul to get the output of the ffn
-        matmul(s->xb, s->hb, w->w2 + l*dim*hidden_dim, hidden_dim, dim);
-
-        // residual connection
-        for (int i = 0; i < dim; i++) {
-            x[i] += s->xb[i];
-        }
+//        // multihead attention. iterate over all heads
+//        int h;
+//        #pragma omp parallel for private(h)
+//        for (h = 0; h < p->n_heads; h++) {
+//            // get the query vector for this head
+//            float* q = s->q + h * head_size;
+//            // attention scores for this head
+//            float* att = s->att + h * p->seq_len;
+//            // iterate over all timesteps, including the current one
+//            for (int t = 0; t <= pos; t++) {
+//                // get the key vector for this head and at this timestep
+//                float* k = s->key_cache + loff + t * kv_dim + (h / kv_mul) * head_size;
+//                // calculate the attention score as the dot product of q and k
+//                float score = 0.0f;
+//                for (int i = 0; i < head_size; i++) {
+//                    score += q[i] * k[i];
+//                }
+//                score /= sqrtf(head_size);
+//                // save the score to the attention buffer
+//                att[t] = score;
+//            }
+//
+//            // softmax the scores to get attention weights, from 0..pos inclusively
+//            softmax(att, pos + 1);
+//
+//            // weighted sum of the values, store back into xb
+//            float* xb = s->xb + h * head_size;
+//            memset(xb, 0, head_size * sizeof(float));
+//            for (int t = 0; t <= pos; t++) {
+//                // get the value vector for this head and at this timestep
+//                float* v = s->value_cache + loff + t * kv_dim + (h / kv_mul) * head_size;
+//                // get the attention weight for this timestep
+//                float a = att[t];
+//                // accumulate the weighted value into xb
+//                for (int i = 0; i < head_size; i++) {
+//                    xb[i] += a * v[i];
+//                }
+//            }
+//        }
+//
+//        // final matmul to get the output of the attention
+//        matmul(s->xb2, s->xb, w->wo + l*dim*dim, dim, dim);
+//
+//        // residual connection back into x
+//        for (int i = 0; i < dim; i++) {
+//            x[i] += s->xb2[i];
+//        }
+//
+//        // ffn rmsnorm
+//        rmsnorm(s->xb, x, w->rms_ffn_weight + l*dim, dim);
+//
+//        // Now for FFN in PyTorch we have: self.w2(F.silu(self.w1(x)) * self.w3(x))
+//        // first calculate self.w1(x) and self.w3(x)
+//        matmul(s->hb, s->xb, w->w1 + l*dim*hidden_dim, dim, hidden_dim);
+//        matmul(s->hb2, s->xb, w->w3 + l*dim*hidden_dim, dim, hidden_dim);
+//
+//        // SwiGLU non-linearity
+//        for (int i = 0; i < hidden_dim; i++) {
+//            float val = s->hb[i];
+//            // silu(x)=x*σ(x), where σ(x) is the logistic sigmoid
+//            val *= (1.0f / (1.0f + expf(-val)));
+//            // elementwise multiply with w3(x)
+//            val *= s->hb2[i];
+//            s->hb[i] = val;
+//        }
+//
+//        // final matmul to get the output of the ffn
+//        matmul(s->xb, s->hb, w->w2 + l*dim*hidden_dim, hidden_dim, dim);
+//
+//        // residual connection
+//        for (int i = 0; i < dim; i++) {
+//            x[i] += s->xb[i];
+//        }
     }
 
-    // final rmsnorm
-    rmsnorm(x, x, w->rms_final_weight, dim);
-
-    // classifier into logits
-    matmul(s->logits, x, w->wcls, p->dim, p->vocab_size);
-    return s->logits;
+//    // final rmsnorm
+//    rmsnorm(x, x, w->rms_final_weight, dim);
+//
+//    // classifier into logits
+//    matmul(s->logits, x, w->wcls, p->dim, p->vocab_size);
+    return s->logits.data();
 }
+
+#if 0
 
 #endif
 
@@ -837,16 +878,22 @@ void generate(Transformer<T> *transformer, Tokenizer<T> *tokenizer, Sampler<T> *
         exit(EXIT_FAILURE);
     }
 
-//    // start the main loop
-//    long start = 0;  // used to time our code, only initialized after first iteration
-//    int next;        // will store the next token in the sequence
-//    int token = prompt_tokens[0]; // kick off with the first token in the prompt
-//    int pos = 0;     // position in the sequence
-//    while (pos < steps) {
-//
-//        // forward the transformer to get logits for the next token
-//        float* logits = forward(transformer, token, pos);
-//
+    // start the main loop
+    long start = 0;  // used to time our code, only initialized after first iteration
+    int next;        // will store the next token in the sequence
+    int token = prompt_tokens[0]; // kick off with the first token in the prompt
+    int pos = 0;     // position in the sequence
+    while (pos < steps) {
+
+        // forward the transformer to get logits for the next token
+        float* logits = forward(transformer, token, pos);
+        {
+            static int i = 0;
+            if(i%100000 == 0)
+                std::cout << "    MILLAD:   preventing from being removed" << logits[i%1000] << std::endl;
+            i++;
+        }
+
 //        // advance the state machine
 //        if (pos < num_prompt_tokens - 1) {
 //            // if we are still processing the input prompt, force the next prompt token
@@ -855,7 +902,7 @@ void generate(Transformer<T> *transformer, Tokenizer<T> *tokenizer, Sampler<T> *
 //            // otherwise sample the next token from the logits
 //            next = sample(sampler, logits);
 //        }
-//        pos++;
+        pos++;
 //
 //        // data-dependent terminating condition: the BOS (=1) token delimits sequences
 //        if (next == 1) { break; }
@@ -868,9 +915,9 @@ void generate(Transformer<T> *transformer, Tokenizer<T> *tokenizer, Sampler<T> *
 //
 //        // init the timer here because the first iteration can be slower
 //        if (start == 0) { start = time_in_ms(); }
-//    }
+    }
 //    printf("\n");
-//
+
 //    // report achieved tok/s (pos-1 because the timer starts after first iteration)
 //    if (pos > 1) {
 //        long end = time_in_ms();
